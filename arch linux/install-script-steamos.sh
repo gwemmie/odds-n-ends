6140541c7349e58975c8ac506ad9dbbfad5b47c7
James Bove <jebove@ucsc.edu>
2016-07-19 16:07:23 -0700

    updated arch linux install scripts

--- arch linux/install-script-steamos.sh
+++ arch linux/install-script-steamos.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 BACKUP=$HOME/Backup/Computers/Linux
+INFO=$HOME/Dropbox/Settings/Scripts
+ROUTER=$(sed -n 1p $INFO/$(sed -n 1p $INFO/ROUTER).info) # hostname of main computer & router
 set -e
 echo "ADD TO /etc/fstab"
 echo "home	/home     	          9p      	trans=virtio	0 0"
@@ -10,14 +12,17 @@ sleep 5
 echo "INSTALLING PACKAGES..."
 sleep 2
 set -x
+sudo cp $BACKUP/pkg/$ROUTER/*libgl*.tar.xz /var/cache/pacman/pkg/
+sudo cp $BACKUP/pkg/$(hostname)/*libgl*.tar.xz /var/cache/pacman/pkg/
 sudo pacman -Sy libgl lib32-libgl
 sudo pacman -Rs gcc gcc-libs
-mkdir -p $BACKUP/pkg/temp
-cp $BACKUP/pkg/Death-Tower/*.tar.xz $BACKUP/pkg/temp/
-cp $BACKUP/pkg/$(hostname)/*.tar.xz $BACKUP/pkg/temp/
-sudo pacman -U --noconfirm $BACKUP/pkg/temp/pacaur*.tar.xz
-PKGDEST=$BACKUP/pkg/temp pacaur -S --noconfirm --noedit --needed $(cat $BACKUP/pkg/$(hostname)/list)
-rm -r $BACKUP/pkg/temp
+sudo pacman -U --noconfirm $BACKUP/pkg/$ROUTER/pacaur*.tar.xz
+sudo cp -n $BACKUP/pkg/$(hostname)/*.tar.xz /var/cache/pacman/pkg/
+sudo cp -n $BACKUP/pkg/$ROUTER/*.tar.xz /var/cache/pacman/pkg/
+sudo chmod 777 /var/cache/pacman/pkg
+PKGDEST=/var/cache/pacman/pkg pacaur -S --noconfirm --noedit --needed $(<$BACKUP/pkg/$(hostname)/list)
+sudo chmod 755 /var/cache/pacman/pkg
+sudo pacman -D --asdeps $(<$BACKUP/pkg/$(hostname)/deps)
 
 set +x
 echo "SETTING UP SYSTEM..."
@@ -30,7 +35,7 @@ sudo systemctl enable xlogin@$USER.service
 sudo systemctl enable sshd.socket
 sudo systemctl enable x2goserver.service
 sudo sed -i 's/my $lines=`ss -lxu`;/my $lines=`ss -lx`;/' /usr/bin/x2golistdesktops
-sudo cp $BACKUP/bin/local/* /usr/local/bin/
+sudo cp -d $BACKUP/bin/local/* /usr/local/bin/
 sudo cp $BACKUP/conf/9p-virtio.conf /etc/modules-load.d/
 sudo cp $HOME/Backup/Consoles/Wii\ \&\ Gamecube/dsp_*.bin /usr/share/dolphin-emu/sys/GC/
 sudo cp $BACKUP/conf/51-gcadapter.rules /etc/udev/rules.d/
--- arch linux/install-script.sh
+++ arch linux/install-script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 BACKUP=$HOME/Backup/Computers/Linux
+INFO=$HOME/Dropbox/Settings/Scripts
+ROUTER=$(sed -n 1p $INFO/$(sed -n 1p $INFO/ROUTER).info) # hostname of main computer & router
 set -e
 echo "MAKE SURE YOU'RE IN WHEEL GROUP AND CAN USE SUDO AND HAVE INTERNET"
 sleep 5
@@ -7,20 +9,29 @@ sleep 5
 echo "INSTALLING PACKAGES..."
 sleep 2
 set -x
-sudo cp $BACKUP/pkg/$(hostname)/*libgl*.tar.xz /var/cache/pacman/pkg/
+sudo cp $BACKUP/pkg/$ROUTER/*libgl*.tar.xz /var/cache/pacman/pkg/
+if [ "$(hostname)" != "$ROUTER" ]
+then sudo cp $BACKUP/pkg/$(hostname)/*libgl*.tar.xz /var/cache/pacman/pkg/
+fi
 sudo pacman -Sy libgl lib32-libgl
 sudo pacman -Rs gcc gcc-libs
-sudo pacman -U --noconfirm $BACKUP/pkg/$(hostname)/pacaur*.tar.xz
-PKGDEST=$BACKUP/pkg/$(hostname) pacaur -S --noconfirm --noedit --needed $(cat $BACKUP/pkg/$(hostname)/list)
+sudo pacman -U --noconfirm $BACKUP/pkg/$ROUTER/pacaur*.tar.xz
 sudo cp -n $BACKUP/pkg/$(hostname)/*.tar.xz /var/cache/pacman/pkg/
+if [ "$(hostname)" != "$ROUTER" ]
+then sudo cp -n $BACKUP/pkg/$ROUTER/*.tar.xz /var/cache/pacman/pkg/
+fi
+sudo chmod 777 /var/cache/pacman/pkg
+PKGDEST=/var/cache/pacman/pkg pacaur -S --noconfirm --noedit --needed $(<$BACKUP/pkg/$(hostname)/list)
+sudo chmod 755 /var/cache/pacman/pkg
+sudo pacman -D --asdeps $(<$BACKUP/pkg/$(hostname)/deps)
 
 set +x
 echo "SETTING UP SYSTEM..."
 sleep 2
 set -x
-sudo cp $BACKUP/bin/local/* /usr/local/bin/
-sudo cp -R $BACKUP/ufw/ufw/* /etc/ufw/
-sudo cp -R $BACKUP/ufw/gufw/* /etc/gufw/
+sudo cp -d $BACKUP/bin/local/* /usr/local/bin/
+sudo cp -dR $BACKUP/ufw/ufw/* /etc/ufw/
+sudo cp -dR $BACKUP/ufw/gufw/* /etc/gufw/
 sudo systemctl enable ufw.service
 sudo ufw enable
 sudo systemctl enable lightdm.service
@@ -30,7 +41,6 @@ sudo systemctl enable bluetooth
 sudo systemctl enable udisks.service
 systemctl --user enable btsync
 echo HandleLidSwitch=ignore | sudo tee -a /etc/systemd/logind.conf
-sudo pacman -S --noconfirm flashplugin
 rm -rf $HOME/.adobe/Flash_Player/{NativeCache,AssetCache,APSPrivateData2}
 sudo cp $BACKUP/conf/alsa-base.conf /etc/modprobe.d/
 sudo mkdir /etc/openal
@@ -41,7 +51,6 @@ sudo cp $BACKUP/conf/uinput.conf /etc/modules-load.d/
 sudo cp $BACKUP/conf/51-gcadapter.rules /etc/udev/rules.d/
 sudo udevadm control --reload-rules
 sudo cp -R $HOME/.config /root/
-sudo cp -R $HOME/.local /root/
 sudo cp $BACKUP/bin/version /usr/bin/version
 sudo cp $HOME/Backup/Consoles/Wii\ \&\ Gamecube/dsp_*.bin $HOME/.dolphin-emu/GC/
 sudo cp $HOME/Backup/Consoles/Wii\ \&\ Gamecube/dsp_*.bin /usr/share/dolphin-emu/sys/GC/
@@ -51,7 +60,16 @@ sudo cp $BACKUP/conf/10-ptrace.conf /etc/sysctl.d/
 sudo cp $BACKUP/conf/60-xwiimote.conf /etc/X11/xorg.conf.d/
 sudo cp $BACKUP/conf/20-custom.conf /etc/X11/xorg.conf.d/
 sudo cp $BACKUP/conf/engrampa.tap /usr/lib/xfce4/thunar-archive-plugin/
-if [ $(hostname) = "Death-Tower" ];
+while read LINE; do
+  LINE="$(echo $LINE | sed 's/\#.*//')"
+  if [ "$LINE" != "" ]; then
+    LINE="$(echo $LINE | sed 's/\W.*//')=$(echo $LINE | sed 's/.*DEFAULT=//')"
+    if ! grep -Fxq "$LINE" /etc/environment ; then
+      echo "$LINE" | sudo tee -a /etc/environment
+    fi
+  fi
+done < $HOME/.pam_environment
+if [ "$(hostname)" = "Death-Tower" ];
 then
   sudo cpupower frequency-set -g performance
   echo "governor='performance'" | sudo tee -a /etc/default/cpupower
@@ -64,17 +82,18 @@ then
   sudo cp $BACKUP/conf/panelfix.service /etc/systemd/system/
   sudo systemctl enable panelfix.service
   sudo sed -i 's/my $lines=`ss -lxu`;/my $lines=`ss -lx`;/' /usr/bin/x2golistdesktops
-  sudo cp $BACKUP/conf/20-gaming-proprietary.conf /etc/X11/xorg.conf.d/
+  sudo cp $BACKUP/conf/10-nouveau.conf /etc/X11/xorg.conf.d/
+  sudo cp $BACKUP/conf/20-amdgpu.conf /etc/X11/xorg.conf.d/
   sudo cp $BACKUP/conf/vfio-pci.conf /etc/modprobe.d/
   sudo cp $BACKUP/conf/qemu.conf /etc/libvirt/
   sudo cp $BACKUP/conf/80-libvirt.rules /etc/polkit-1/rules.d/
   sudo cp $BACKUP/conf/smb.conf /etc/samba/
-  sudo cp -R $BACKUP/qemu/* /etc/libvirt/qemu/
+  sudo cp -dR $BACKUP/qemu/* /etc/libvirt/qemu/
   sudo mkdir -p /etc/libvirt/hooks && sudo cp $BACKUP/bin/qemu /etc/libvirt/hooks/
   yaourt -S --asdeps --noconfirm rpmextract
   cd /tmp
   rpmextract.sh $BACKUP/edk2.git-ovmf-x64-*.rpm
-  sudo cp -R ./usr/share/* /usr/share/
+  sudo cp -a ./usr/share/* /usr/share/
   sudo rm -rf ./usr
   cd
   sudo ln -s $HOME/.card.sh /root/
@@ -92,7 +111,7 @@ sleep 2
 set -x
 sudo groupadd autologin
 sudo usermod -aG disk,storage,network,audio,power,video,optical,wireshark,autologin,lp,sys $USER
-if [ $(hostname) = "Death-Tower" ];
+if [ "$(hostname)" = "Death-Tower" ];
 then
   sudo groupadd libvirtd
   sudo usermod -aG libvirtd,x2godesktopsharing $USER
