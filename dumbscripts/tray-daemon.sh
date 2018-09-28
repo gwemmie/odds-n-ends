80c6ce61c2c4ec3fa50f5d0b80371842c208ea94
James Bove <jebove@ucsc.edu>
2018-09-28 16:10:59 -0700

    generic routine update for Fri Sep 28 16:10:59 PDT 2018

--- dumbscripts/tray-daemon.sh
+++ dumbscripts/tray-daemon.sh
@@ -11,8 +11,10 @@
 #   $HOME/.local/share/icons/<script's normal icon name>-x.<any extension compatible with systray>
 #   Example: for mail-notify.sh, my normal icon is "gmail-offline", so I modified that to have
 #     an X emblem on it, and saved that as $HOME/.local/share/icons/gmail-offline-x.svg
+# I decided to also add a simpler version of the loop for regular daemon-style programs
 LOCATION="$HOME/.dumbscripts" # where scripts live
 SCRIPTS=( mail-notify )
+DAEMONS=( orage )
 declare -A YADPIDS
 
 if ! hash yad 2>/dev/null; then
@@ -30,7 +32,7 @@ while true; do
       fi
       # start error tray icon
       ICON="$(grep "ICON=" "$LOCATION/$SCRIPT.sh" | head -1 | sed 's/.*ICON="//' | sed 's/".*//')-x"
-      yad --notification --text="$SCRIPT is broken, click to restart" --command="$LOCATION/$SCRIPT.sh" --image="$ICON" 2>/dev/null &
+      yad --notification --text="$SCRIPT is off, click to restart" --command="$LOCATION/$SCRIPT.sh" --image="$ICON" 2>/dev/null &
       YADPIDS["$SCRIPT"]=$!
     elif ! ps ${YADPIDS["$SCRIPT"]}
     then unset YADPIDS["$SCRIPT"]
@@ -38,6 +40,19 @@ while true; do
   elif [ ${YADPIDS["$SCRIPT"]+_} ]; then
     kill ${YADPIDS["$SCRIPT"]}
     unset YADPIDS["$SCRIPT"]
+  fi; done
+  for DAEMON in "${DAEMONS[@]}"; do if ! pgrep -x "$DAEMON"; then
+    if ! [ ${YADPIDS["$DAEMON"]+_} ]; then
+      # start error tray icon
+      ICON="system-error"
+      yad --notification --text="$DAEMON is off, click to restart" --command="$DAEMON" --image="$ICON" 2>/dev/null &
+      YADPIDS["$DAEMON"]=$!
+    elif ! ps ${YADPIDS["$DAEMON"]}
+    then unset YADPIDS["$DAEMON"]
+    fi
+  elif [ ${YADPIDS["$DAEMON"]+_} ]; then
+    kill ${YADPIDS["$DAEMON"]}
+    unset YADPIDS["$DAEMON"]
   fi; done
   sleep 1
 done
