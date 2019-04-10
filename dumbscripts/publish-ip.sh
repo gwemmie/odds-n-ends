66f725f3be9920e61b566f7871df6abdab3b076c
James Bove <jebove@ucsc.edu>
2019-04-10 06:23:56 -0700

    generic routine update for Wed Apr 10 06:23:56 PDT 2019

--- dumbscripts/publish-ip.sh
+++ dumbscripts/publish-ip.sh
@@ -3,7 +3,10 @@
 INFO=$HOME/Dropbox/Settings/Scripts
 ROUTER=$(sed -n 1p $INFO/ROUTER) # hostname of main computer & router
 EXIP=$(sed -n 2p $INFO/$ROUTER.info) # public IP on last boot
-/usr/bin/ifconfig $(ip route ls | grep 'default via' | head -1 | awk '{ print $5}') | grep 'inet ' | awk '{ print $2}' > $INFO/$(hostname).info
+if ! [ -e "$INFO/$(hostname).info" ]
+then touch "$INFO/$(hostname).info"
+fi
+sed -i "1s|.*|$(/usr/bin/ifconfig $(ip route ls | grep 'default via' | head -1 | awk '{ print $5}') | grep 'inet ' | awk '{ print $2}')|" $INFO/$(hostname).info
 
 NEWEXIP=$(/usr/bin/dig +short myip.opendns.com @resolver1.opendns.com)
 if [ -z "$NEWEXIP" ]
@@ -14,5 +17,8 @@ else
   if [ "$EXIP" != "$NEWEXIP" ]
   then notify-send -u critical -t 300000 "public IP has changed from $EXIP to $NEWEXIP"
   fi
-  echo $NEWEXIP >> $INFO/$(hostname).info
+  if [ -z "$(sed -n 2p < "$INFO/$(hostname).info")" ]
+  then echo "$NEWEXIP" >> $INFO/$(hostname).info
+  else sed -i "2s|.*|$NEWEXIP|" $INFO/$(hostname).info
+  fi
 fi
